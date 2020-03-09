1597fa78d85c32ac568e496905df6c4100317d71
jiwook2000 <56825960+jiwook2000@users.noreply.github.com>
2020-03-09 22:17:11 +0900

    final

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.animation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout page;
+    Animation leftAnimation;
+    Animation rightAnimation;
+    boolean isPageOpen=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        page = findViewById(R.id.page);
+        leftAnimation=AnimationUtils.loadAnimation(this,R.anim.translate_left);
+        rightAnimation=AnimationUtils.loadAnimation(this,R.anim.translate_right);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isPageOpen) {
+                    page.startAnimation(rightAnimation);
+                    isPageOpen=false;
+                }else{
+                    page.startAnimation(leftAnimation);
+                    page.setVisibility(View.VISIBLE);
+                    isPageOpen=true;
+                }
+            }
+        });
+
+        MyAnimationListener animationListener=new MyAnimationListener();
+        leftAnimation.setAnimationListener(animationListener);
+    }
+    private class MyAnimationListener implements Animation.AnimationListener{
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            page.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
